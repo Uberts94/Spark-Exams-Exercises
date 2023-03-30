5c21c4cc8b81faa9b4c0ebe3002f98e4b7610391
Uberts94 <s292522@studenti.polito.it>
2023-03-30 16:56:01 +0200

    first task finished and locally tested

--- Es43/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Es43/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,6 +1,9 @@
 package it.polito.bigdata.spark.example;
 
 import org.apache.spark.api.java.*;
+
+import scala.Tuple2;
+
 import org.apache.spark.SparkConf;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -14,11 +17,15 @@ public class SparkDriver {
 		Logger.getLogger("akka").setLevel(Level.OFF);
 
 
-		String inputPath;
+		String inputPath1;
+		String inputPath2;
 		String outputPath;
+		int threshold;
 		
-		inputPath=args[0];
-		outputPath=args[1];
+		inputPath1=args[0];
+		inputPath2=args[1];
+		outputPath=args[2];
+		threshold=Integer.parseInt(args[3]);
 
 	
 		// Create a configuration object and set the name of the application
@@ -34,8 +41,26 @@ public class SparkDriver {
 
 		
 		// Read the content of the input file/folder
-		JavaRDD<String> inputRDD = sc.textFile(inputPath);
-
+		JavaRDD<String> readingsRDD = sc.textFile(inputPath1);
+		
+		JavaRDD<String> criticalStationPerc = readingsRDD.mapToPair(line -> {
+			String[] reading = line.split(",");
+			if(Integer.parseInt(reading[5]) < threshold) 
+				return new Tuple2<String, Tuple2<Integer, Integer>>(reading[0], new Tuple2<Integer, Integer>(1, 1));
+			else return new Tuple2<String, Tuple2<Integer, Integer>>(reading[0], new Tuple2<Integer, Integer>(0, 1));
+		}).reduceByKey((e1, e2) -> {
+			return new Tuple2<Integer, Integer>(e1._1()+e2._1(), e1._2()+e2._2());
+		}).mapToPair(e -> {
+			return new Tuple2<Float, String>((float) e._2()._1()/e._2()._2(), e._1());
+		}).filter(e -> {
+			if (e._1 > 0.2) return true;
+			else return false;
+		}).sortByKey(false).map(e -> {
+			return e._2();
+		});
+        
+		criticalStationPerc.saveAsTextFile(outputPath);
+		
 		// Close the Spark context
 		sc.close();
 	}
