1ec98ec364c6f65b73caff1231500c45f27b226f
Uberts94 <s292522@studenti.polito.it>
2023-03-30 12:53:49 +0200

    finished and locally tested

--- Es37/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Es37/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,6 +1,9 @@
 package it.polito.bigdata.spark.example;
 
 import org.apache.spark.api.java.*;
+
+import scala.Tuple2;
+
 import org.apache.spark.SparkConf;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -36,7 +39,15 @@ public class SparkDriver {
 		// Read the content of the input file/folder
 		JavaRDD<String> inputRDD = sc.textFile(inputPath);
 
+		JavaPairRDD<String, Float> outputRDD = inputRDD.mapToPair(line -> {
+			String[] sensor = line.split(",");
+			return new Tuple2<String, Float>(sensor[0], Float.parseFloat(sensor[2]));
+		}).reduceByKey((e1, e2) -> {
+			if(e1 > e2) return e1;
+			else return e2;
+		});
 		
+		outputRDD.saveAsTextFile(outputPath);
 
 		// Close the Spark context
 		sc.close();
