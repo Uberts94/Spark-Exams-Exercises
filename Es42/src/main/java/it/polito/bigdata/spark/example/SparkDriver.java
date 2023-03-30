cddf30ede31e950e9db29f294f1aa63e63a751a9
Uberts94 <s292522@studenti.polito.it>
2023-03-30 16:12:59 +0200

    finished and locally tested

--- Es42/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Es42/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,7 +1,14 @@
 package it.polito.bigdata.spark.example;
 
 import org.apache.spark.api.java.*;
+
+import scala.Tuple2;
+
 import org.apache.spark.SparkConf;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 	
@@ -40,8 +47,29 @@ public class SparkDriver {
 		JavaRDD<String> input1RDD = sc.textFile(inputPath1);
 		JavaRDD<String> input2RDD = sc.textFile(inputPath2);
 		
+		// Questions
+		JavaPairRDD <String, String> questionsRDD = input1RDD.mapToPair(line -> {
+			String[] question = line.split(",");
+			return new Tuple2<String, String>(question[0], question[2]);
+		});
 		
+		// Answers
+		JavaPairRDD <String, String> answersRDD = input2RDD.mapToPair(line -> {
+			String[] answer = line.split(",");
+			return new Tuple2<String, String>(answer[1], answer[3]);
+		});
 
+		JavaPairRDD <String, Tuple2<String, List<String>>> outputRDD = questionsRDD.join(answersRDD).mapToPair(e -> {
+			List<String> answer = new ArrayList<String>();
+			answer.add(e._2()._2());
+			return new Tuple2<String, Tuple2<String, List<String>>>(e._1(), new Tuple2<String, List<String>>(e._2()._1(), answer));
+		}).reduceByKey((e1, e2) -> {
+			if(!e2._2().isEmpty()) e1._2.add(e2._2.get(0));
+			return e1;
+		});
+		
+		outputRDD.saveAsTextFile(outputPath);
+		
 		// Close the Spark context
 		sc.close();
 	}
