7b10acd6f8be456604f758d6d318d73f29932f74
Uberts94 <s292522@studenti.polito.it>
2023-03-30 15:42:23 +0200

    adding base project

---
+++ Es42/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -0,0 +1,48 @@
+package it.polito.bigdata.spark.example;
+
+import org.apache.spark.api.java.*;
+import org.apache.spark.SparkConf;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+	
+public class SparkDriver {
+	
+	public static void main(String[] args) {
+
+		// The following two lines are used to switch off some verbose log messages
+		Logger.getLogger("org").setLevel(Level.OFF);
+		Logger.getLogger("akka").setLevel(Level.OFF);
+
+
+		String inputPath1;
+		String inputPath2;
+		String outputPath;
+		
+		inputPath1=args[0];
+		inputPath2=args[1];
+		outputPath=args[2];
+
+
+	
+		// Create a configuration object and set the name of the application
+		SparkConf conf=new SparkConf().setAppName("Es42").setMaster("local");
+		
+		// Use the following command to create the SparkConf object if you want to run
+		// your application inside Eclipse.
+		// Remember to remove .setMaster("local") before running your application on the cluster
+		// SparkConf conf=new SparkConf().setAppName("Es42").setMaster("local");
+		
+		// Create a Spark Context object
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		
+		// Read the content of the input file/folder
+		JavaRDD<String> input1RDD = sc.textFile(inputPath1);
+		JavaRDD<String> input2RDD = sc.textFile(inputPath2);
+		
+		
+
+		// Close the Spark context
+		sc.close();
+	}
+}
