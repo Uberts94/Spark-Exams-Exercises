cb921a94cd472564afe5dc445b3401810ead17ef
Uberts94 <s292522@studenti.polito.it>
2023-04-04 17:36:18 +0200

    finished and locally tested

---
+++ Es34Dataframe/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -0,0 +1,37 @@
+package it.polito.bigdata.spark.example;
+
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.max;
+	
+public class SparkDriver {
+	
+	public static void main(String[] args) {
+
+
+		String inputPath;
+		String outputPath;
+		
+		inputPath=args[0];
+		outputPath=args[1];
+		
+		SparkSession ss = SparkSession.builder().master("local").appName("Es32 - dataset").getOrCreate();
+		
+		DataFrameReader dfr = ss.read().option("delimiter", ",").format("csv").option("header", false)
+								.option("inferSchema", true);
+		
+		Dataset<Row> df = dfr.load(inputPath).selectExpr("_c0 as sensorId", "_c1 as timestamp", "_c2 as value");
+		
+		Double max = df.select("value").agg(max("value")).first().getDouble(0);
+		
+		df.filter(row -> {
+			if(row.getDouble(2) == max) return true;
+			else return false;
+		}).write().format("csv").option("header", true).save(outputPath);
+		
+		// Close the Spark context
+		ss.close();
+	}
+}
