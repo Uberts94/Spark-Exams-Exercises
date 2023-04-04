7b9f98997f69a3eb9bdbd1a638b743fb8fb73293
Uberts94 <s292522@studenti.polito.it>
2023-04-04 16:59:47 +0200

    finished and locally tested

--- Es32Dataset/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Es32Dataset/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,6 +1,10 @@
 package it.polito.bigdata.spark.example;
 
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.max;
 	
 public class SparkDriver {
 	
@@ -8,16 +12,19 @@ public class SparkDriver {
 
 
 		String inputPath;
-		String outputPath;
-		String prefix;
 		
 		inputPath=args[0];
-		outputPath=args[1];
-		prefix=args[2];
-
-	
+		
 		SparkSession ss = SparkSession.builder().master("local").appName("Es32 - dataset").getOrCreate();
-
+		
+		DataFrameReader dfr = ss.read().option("delimiter", ",").format("csv").option("header", false)
+								.option("inferSchema", true);
+		
+		Dataset<Reading> readings = dfr.load(inputPath).as(Encoders.bean(Reading.class));
+		
+		Dataset<Double> maxValueDF = readings.agg(max("_c2")).as(Encoders.DOUBLE());
+		
+		System.out.println(maxValueDF.first());
 		// Close the Spark context
 		ss.close();
 	}
