da693423297779746200594e71fb147c0d1df023
Uberts94 <s292522@studenti.polito.it>
2023-04-05 10:51:24 +0200

    adding base project

---
+++ Es48Dataframe/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -0,0 +1,20 @@
+package it.polito.bigdata.spark.example;
+
+import org.apache.spark.sql.SparkSession;
+	
+public class SparkDriver {
+	
+	public static void main(String[] args) {
+
+
+		String inputPath;
+		
+		inputPath=args[0];
+		
+		SparkSession ss = SparkSession.builder().master("local").appName("Es48 - dataframe").getOrCreate();
+		
+
+		// Close the Spark context
+		ss.close();
+	}
+}
