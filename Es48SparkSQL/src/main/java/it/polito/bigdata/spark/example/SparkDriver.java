992b9715efb502f91434fa4ef66f470877789905
Uberts94 <s292522@studenti.polito.it>
2023-04-05 12:03:02 +0200

    finished and locally tested

--- Es48SparkSQL/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
+++ Es48SparkSQL/src/main/java/it/polito/bigdata/spark/example/SparkDriver.java
@@ -1,5 +1,7 @@
 package it.polito.bigdata.spark.example;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 	
 public class SparkDriver {
@@ -8,13 +10,28 @@ public class SparkDriver {
 
 
 		String inputPath;
+		String outputPath;
 		
 		inputPath=args[0];
+		outputPath=args[1];
 		
 		SparkSession ss = SparkSession.builder().master("local").appName("Es48 - SparkSQL").getOrCreate();
 		
-
-		// Close the Spark context
+		Dataset<Row> persons = ss.read().option("delimiter", ",").format("csv")
+								 .option("header", true).option("inferSchema", true).load(inputPath);
+		
+		persons.createOrReplaceTempView("people");
+		
+		Dataset<Row> personsAvg = ss.sql(""
+				+ "SELECT name, avg(age) as avgAge, count(*) as counter "
+				+ "FROM people "
+				+ "GROUP BY name")
+				.filter(row -> row.getLong(row.fieldIndex("counter")) > 1)
+				.select("name", "avgAge");
+		
+		personsAvg.javaRDD().coalesce(1).saveAsTextFile(outputPath);
+		
+		// Close the Spark session
 		ss.close();
 	}
 }
